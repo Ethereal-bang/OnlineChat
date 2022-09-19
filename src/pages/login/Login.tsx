deea2eb4d9b97e91c1f1f2cba7b79e4927f9c309
Ethereal-bang <939847757@qq.com>
2022-09-19 17:39:10 +0800

    update: Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,35 @@
+import {Button, Form, Input, Space} from "antd";
+import styles from "./Login.module.scss";
+
 export const Login = () => {
     return (
-        <h1>Login</h1>
+        <div>
+            <Form className={styles["form"]}>
+                <Form.Item
+                    label={"账号"}
+                    name={"id"}
+                    rules={[{ type: "number", required: true, }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label={"密码"}
+                    name={"password"}
+                    rules={[{ required: true, }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item>
+                    <Space>
+                        <Button type="primary" htmlType="submit">
+                            登录
+                        </Button>
+                        <Button type="primary" htmlType="submit">
+                            注册
+                        </Button>
+                    </Space>
+                </Form.Item>
+            </Form>
+        </div>
     )
 }
