727997f13ebcf73d15ea7da1f59aa4b7208b22a2
Ethereal-bang <939847757@qq.com>
2022-09-19 16:13:29 +0800

    update: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Communicate, Home, Login} from "./pages";
 
 function App() {
-  return (
-    <div />
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Home />} />
+                <Route path={"/login"} element={<Login />} />
+                <Route path={"/communicate"} element={<Communicate />} />
+                <Route path={"*"} element={<h1>404</h1>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/communicate/Communicate.tsx
@@ -0,0 +1,5 @@
+export const Communicate = () => {
+    return (
+        <h1>Communicate</h1>
+    )
+}
