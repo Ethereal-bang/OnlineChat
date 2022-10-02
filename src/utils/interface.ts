179d198fcd51eb860bf12b1f301745a39d4390fe
Ethereal-bang <939847757@qq.com>
2022-10-02 22:39:12 +0800

    fix: contact/search 搜索ID返回对象

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -7,6 +7,7 @@ export interface User {
 }
 
 export interface Contact extends Exclude<User, "password">{
+    uid: number,
     score?: number,  // 亲密度
-    state?: 0 | 1 | 2 | 3 | 4,   // 申请中 已同意 已拒绝 已屏蔽 被屏蔽
+    state?: -1 | 0 | 1 | 2 | 3 | 4,   // 未添加 申请中 已同意 已拒绝 已屏蔽 待处理(对方发送的申请
 }
