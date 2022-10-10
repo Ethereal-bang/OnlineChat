eccd81152028fc0af129326fd418ec798e6a9e9c
Ethereal-bang <939847757@qq.com>
2022-10-10 20:57:17 +0800

    fix: emoji处理时空字符串的情况

--- src/utils/emojiHandle.ts
+++ src/utils/emojiHandle.ts
@@ -1,14 +1,14 @@
 import emojiRegex from "emoji-regex";
 
-export const encodeEmoji = (str: string) => {
+export const encodeEmoji = (str: string | undefined): string => {
     const regex = emojiRegex(); // 匹配emoji的正则
-    return str.replace(regex, p => `emoji(${p.codePointAt(0)})`);
+    return str?.replace(regex, p => `emoji(${p.codePointAt(0)})`) || "";
 }
 
-export const decodeEmoji = (str: string) => {
+export const decodeEmoji = (str: string | undefined): string => {
     const emojiDecodeRegex = /emoji\(\d+\)/g;
-    return str.replace(emojiDecodeRegex, p => {
+    return str?.replace(emojiDecodeRegex, p => {
         // 去掉格式emoji()
         return String.fromCodePoint(Number(p.replace(/[^\d]/g, '')));
-    })
+    }) || "";
 }
